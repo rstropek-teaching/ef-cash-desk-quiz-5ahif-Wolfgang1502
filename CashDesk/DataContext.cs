4e0510f6092eda49280304cfbbba6c33ce80975b
Wolfgang <baum.haus123@gmx.at>
2018-01-10 10:20:53 +0100

    Solution

---
+++ CashDesk/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using CashDesk.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashDesk
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Deposit> Deposits { get; set; }
+   //     public DbSet<DepositStatistics> DepositStatistics { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseInMemoryDatabase("CashDesk");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>()
+                .HasIndex(i => i.LastName)
+                .IsUnique();
+        }
+
+    }
+}
---
+++ CashDesk/Model/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CashDesk.Model
+{
+    public class Member : IMember
+    {
+
+        //public Member(string firstName, string lastName, DateTime birthday)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //    Birthday = birthday;
+        //}
+
+        [Key]
+        public int MemberNumber { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+    }
+}
